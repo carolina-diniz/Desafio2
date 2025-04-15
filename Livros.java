52bfd31919d2d2319e3ec90ca456523292e831ac
Maria Eduarda Aragão Farias <madufarias47@gmail.com>
2025-04-15 10:52:07 -0400

    ListaCheck

--- Livros.java
+++ Livros.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Livros {
+    static ArrayList<String> livros = new ArrayList<String>();
+    static Scanner scanner = new Scanner(System.in);
+
     static String Operacao() {
         String operacao = """
                 
@@ -19,47 +22,46 @@ public class Livros {
         return operacao;
     }
 
-    static String Adicionar() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite nome do livro e autor: ");
+    static String inputlinha() {
         return scanner.nextLine();
     }
 
-    //static void Listar() {}
-
-    //static void Editar() {}
-
-    //static void Remover() {}
+    static int inputint() {
+        return scanner.nextInt();
+    }
 
     static void Execucao() {
-        ArrayList<String> livros = new ArrayList<String>();
-        Scanner scanner = new Scanner(System.in);
-
         while (true) {
             System.out.println(Operacao());
             System.out.println("Digite um número: ");
-            int operacaoi = scanner.nextInt();
+            int operacaoi = inputint();
 
             if (operacaoi == 1) {
-                String livro = Adicionar();
-                livros.add(livro);
+                System.out.print("Digite nome do livro e autor: ");
+                String novolivro = inputlinha();
+                livros.add(novolivro);
             }
-            // else if (operacaoi == 2) {
-            // }
+            else if (operacaoi == 2) {
+                for (int i = 0; i < livros.size(); i++) {
+                    System.out.print((i + 1) + ". " + livros.get(i) + "\n");
+                }
+            }
 
-            // else if (operacaoi == 3) {
-            // }
+            else if (operacaoi == 3) {
+                for (int i = 0; i < livros.size(); i++) {
+                    System.out.print((i + 1) + ". " + livros.get(i) + "\n");
+                }
+            }
 
             // else if (operacaoi == 4) {
             // }
 
             else if (operacaoi == 5){
-                System.out.println("Obrigado por usar nosso sistema, operação finalizada");
+                System.out.println("Operação finalizada, Obrigado por usar nosso sistema!");
                 break;
             }
             else {
                 System.out.println("Número inválido, tente outra operação");
-                continue;
             }
         }
     }
@@ -71,3 +73,8 @@ public class Livros {
 
 //Lista e scanner fora do looping para não criar novos
 //Pode retornar o scanner
+//Lista pode fora dos métodos se colocar static na frente, assim como o scanner
+// só static torna o elemento global
+//Como tornar em um método a estrutura:
+// for (int i = 0; i < livros.size(); i++) {
+//     System.out.print((i + 1) + ". " + livros.get(i) + "\n");
